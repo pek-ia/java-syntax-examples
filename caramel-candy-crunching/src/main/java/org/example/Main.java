866e92e69efe51d1648b71bebb2cf66dea2bb018
Paul Kimball <pekimball@interfaceassociates.net>
2023-10-17 11:29:37 -0400

    Caramel velocity example

---
+++ caramel-candy-crunching/src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!  How fast do I snack?");
+
+        double numCaramels = 29.;
+        double numHours = 15;
+        double caramelsPerHour = numCaramels/numHours;
+        System.out.println(caramelsPerHour);
+        System.out.printf("%.2f", caramelsPerHour);
+    }
+}
