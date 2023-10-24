5ef0460e5b35175a86295e3d0e0e06131bd6c741
Paul Kimball <pekimball@interfaceassociates.net>
2023-10-24 10:04:15 -0400

    read donut from csv file

--- dealing-with-donuts/src/main/java/org/example/Donut.java
+++ dealing-with-donuts/src/main/java/org/example/Donut.java
@@ -1,7 +1,23 @@
 package org.example;
 
 public class Donut {
+
     private String name;
     private int calories;
     private double price;
+
+    @Override
+    public String toString() {
+        return "Donut{" +
+                "name='" + name + '\'' +
+                ", calories=" + calories +
+                ", price=" + price +
+                '}';
+    }
+
+    public Donut(String name, int calories, double price) {
+        this.name = name;
+        this.calories = calories;
+        this.price = price;
+    }
 }
--- dealing-with-donuts/src/main/java/org/example/Main.java
+++ dealing-with-donuts/src/main/java/org/example/Main.java
@@ -1,11 +1,34 @@
 package org.example;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello donuts!");
 
         // Create a donut
-        Donut d = new Donut();
+        Donut d = null;
+
+        // Read a donut
+        BufferedReader donutFileReader = null;
+        try {
+            donutFileReader = new BufferedReader(new FileReader("donuts.csv"));
+            String donutString = donutFileReader.readLine();
+            String[] donutData = donutString.split("\\|");
+            // Convert each string to the correct type
+            String name = donutData[0];
+            int calories = Integer.parseInt(donutData[1]);
+            double price = Double.parseDouble(donutData[2]);
+
+            // Time to make the donut!
+            d = new Donut(name, calories, price);
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
 
         // Print a donut
         System.out.println(d);
