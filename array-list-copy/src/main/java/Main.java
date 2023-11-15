5beb59a9febf0b0316c190506c35b2456e818730
Paul Kimball <90878065+pek-ia@users.noreply.github.com>
2023-11-15 08:52:48 -0500

    Update Main.java - remove unused variable

--- array-list-copy/src/main/java/Main.java
+++ array-list-copy/src/main/java/Main.java
@@ -11,7 +11,6 @@ import java.util.ArrayList;
  */
 public class Main {
     public static void main(String[] args) {
-        int x;
 
         // An array list
         ArrayList<Vehicle> vehicles = new ArrayList<>();
