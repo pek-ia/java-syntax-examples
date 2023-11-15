3c6810ac7f247ccd75725b3b94a71d3330b7de66
Paul Kimball <pekimball@interfaceassociates.net>
2023-11-15 08:47:36 -0500

    Update main to prove a point

--- array-list-copy/src/main/java/Main.java
+++ array-list-copy/src/main/java/Main.java
@@ -1,12 +1,23 @@
 import java.util.ArrayList;
 
+/*
+ * This is a little experiment to help us figure out the
+ * behavior of the ArrayList.  The question:
+ *
+ *   "Does the ArrayList perform a DEEP copy or a SHALLOW
+ *    copy when making a copy of another list?"
+ *
+ *
+ */
 public class Main {
     public static void main(String[] args) {
+        int x;
 
+        // An array list
         ArrayList<Vehicle> vehicles = new ArrayList<>();
-
         vehicles.add(new Vehicle(1234, "red", "Charger"));
 
+        // Another list makes a copy of the first one
         ArrayList<Vehicle> vehiclesCopy = new ArrayList<>(vehicles);
 
         // Now, the test!!!
